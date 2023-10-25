4bef4d5aa61f7100e1d2e1030e99a357b9ab2ae4
Mathieu Ledru <mathieu@darkwood.fr>
2023-10-25 14:00:49 +0200

    :sparkles: Add read strategy

--- sociallike.user.js
+++ sociallike.user.js
@@ -10,27 +10,50 @@
 // @license      MIT
 // ==/UserScript==
 
+"use strict";
+
 // autolike what you read
 
-(function() {
-    'use strict';
+((readStrategy, timeout = 1000) => {
+    var readStrategies = {
+        'greedy': (readSelector) => {
+            return readSelector
+        },
+        'random': (readSelector) => {
+            return Math.random() >= 0.5 ? readSelector : null
+        }
+    }
 
     var functions = {
-        likeClick() {
-            document.querySelectorAll([
+        likeClick(likeSelector) {
+            likeSelector.forEach((e) => {e.click()})
+        },
+        likeSelector(readSelector) {
+            return [...readSelector].map(element => element.querySelectorAll([
                 '[role="button"][data-testid="like"]', // twitter
-                '.section.file-page__video button.button-like:not(.button--fire)', // odysee
+                '.file-page__video button.button-like:not(.button--fire)', // odysee
                 '#below #segmented-like-button button[aria-pressed="false"]', // youtube
-            ].join(', ')).forEach((e) => {e.click()})
+            ].join(', '))).flat().reduce((acc, nodeList) => acc.concat([...nodeList]), [])
+        },
+        readSelector() {
+            return document.querySelectorAll([
+                'article', // twitter
+                '.file-page__video', // odysee
+                '#primary-inner', // youtube
+            ].join(', '))
         },
         like() {
-            functions.likeClick()
+            let readSelector = functions.readSelector();
+            readSelector = readStrategies[readStrategy](readSelector);
+            if(readSelector) {
+                functions.likeClick(functions.likeSelector(readSelector));
+            }
 
             setTimeout(() => {
                 functions.like()
-            }, 1000)
+            }, timeout)
         },
     }
 
     functions.like()
-})();
+})('greedy');
